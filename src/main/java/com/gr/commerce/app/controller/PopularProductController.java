a399f6101b78c911c5a98fc3987d7ab4367ad491
DamiSoh <83524438+DamiSoh@users.noreply.github.com>
2022-08-07 23:17:51 +0900

    220807-main page api modified

--- src/main/java/com/gr/commerce/app/controller/PopularProductController.java
+++ src/main/java/com/gr/commerce/app/controller/PopularProductController.java
@@ -2,6 +2,7 @@ package com.gr.commerce.app.controller;
 
 import com.gr.commerce.app.mapper.PopularProductMapper;
 import com.gr.commerce.app.service.PopularProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/main")
 public class PopularProductController {
 
-    @Autowired
-    PopularProductService popularProductService;
+    private final PopularProductService popularProductService;
 
     @GetMapping("/popularProduct")
     @ResponseBody
--- src/main/java/com/gr/commerce/app/model/PopularProductDto.java
+++ src/main/java/com/gr/commerce/app/model/PopularProductDto.java
@@ -1,10 +1,14 @@
 package com.gr.commerce.app.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PopularProductDto {
     private int id;
     private String itemId;
